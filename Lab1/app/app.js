ee8bbfd3ad408c9cb22b087681616f032f3954a9
BoMa0103 <bengemingg@gmail.com>
2023-02-15 17:53:01 +0200

    'initial'

--- Lab1/app/app.js
+++ Lab1/app/app.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
     res.send(process.env.HELLO);
 });
 
-app.get("/use", (req, res) => {
+app.get("/userr", (req, res) => {
     res.send({name: process.env.NAME, age: process.env.AGE});
 });
 
