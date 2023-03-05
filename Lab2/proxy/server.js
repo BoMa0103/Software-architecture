e9e1abd855c12800a86c69435fa3c25367dbb569
BoMa0103 <bengemingg@gmail.com>
2023-03-05 15:38:03 +0200

    'init'

---
+++ Lab2/proxy/server.js
@@ -0,0 +1,11 @@
+//server.js
+const express = require('express');
+const app = express();
+
+var routing = require('./routing.js')
+
+app.get('/', (req, res) => {
+  routing.routingApi(req, res);
+})
+
+module.exports = app;
---
+++ Lab2/proxy/start.js
@@ -0,0 +1,9 @@
+//start.js
+const app = require('./server.js');
+var config = require('./config.js');
+
+const PORT = config.get('port');
+
+app.listen(PORT, () => {
+    console.log(`Server start at localhost:${PORT}`);
+});
